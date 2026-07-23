e24cad24e8ea6435ef7a36dbc542f996d32da114
Mukesh Gujju <mukeshrock809@gmail.com>
2026-07-23 20:45:05 +0530

    fix: handle unauthenticated users and fetchSignInMethodsForEmail for password reset

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4016746230" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3845750664" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
