e509ec18c1aa3dea7fe5c27761529a8ac3204d9c
Mukesh Gujju <mukeshrock809@gmail.com>
2026-07-25 17:58:45 +0530

    deploy: web release update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1760751311" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3181321126" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
